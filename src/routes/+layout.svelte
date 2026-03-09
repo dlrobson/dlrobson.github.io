7ff50ba8deaa7be24d7e7d45c348cdef0595c48f
Daniel Robson <danr.236@gmail.com>
2026-03-09 11:40:43 +0000

    chore: switch to just use system theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,8 @@
   import '$lib/tokens.css'
   import '../index.css'
   import type { Snippet } from 'svelte'
-  import ThemeToggle from '$lib/components/ThemeToggle.svelte'
 
   let { children }: { children: Snippet } = $props()
 </script>
 
-<ThemeToggle />
 <main>{@render children()}</main>
